43febd2bf4ff91439d5b1ad80da9381fbd2e34f6
crisjaca <Kristian2964@gmail.com>
2025-09-20 14:46:25 -0500

    Aqui aplico la Denominación de Paquete y muestro en pantalla un mesaje con la función JOptionPane

---
+++ javaAsignacion/src/com/fup/asignacion/dateType.java
@@ -0,0 +1,18 @@
+
+package com.fup.asignacion;
+
+import javax.swing.JOptionPane;
+
+public class dateType {
+    public static void main(String[] args){
+        //variable
+        boolean a = true;
+        String message = "MENSAJE DE ALERTA";
+        //Estructura de control if
+        if (a == true) {
+        //  JoptionPane Imprime por interfaz un mensaje
+        JOptionPane.showMessageDialog(null,message);            
+        }
+                
+    }
+}
