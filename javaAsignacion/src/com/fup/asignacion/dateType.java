e2198be7390e82428079ff97635813fbd76b268b
Cristian <Kristian2964@gmail.com>
2025-09-22 09:12:11 -0500

    Se agregaron cambios en el informe y en la aplicación probando los tipos de datos de texto chat y String

--- javaAsignacion/src/com/fup/asignacion/dateType.java
+++ javaAsignacion/src/com/fup/asignacion/dateType.java
@@ -13,6 +13,7 @@ public class dateType {
         //  JoptionPane Imprime por interfaz un mensaje
         JOptionPane.showMessageDialog(null,message);            
         }
+        
         /***
          BYTE
          ***/
@@ -36,5 +37,42 @@ public class dateType {
         message = "El numero entero positivo es: ";
         int numero = 144;
         JOptionPane.showMessageDialog(null, message+numero);
+        
+        /****
+         *  FLOAT
+         ****/
+        float numero1 = 8.8f;
+        JOptionPane.showMessageDialog(null, numero1);
+        /****
+         *  DOUBLE
+         ****/
+        double numero2 = 8.85443532;
+        JOptionPane.showMessageDialog(null, numero2);
+        
+        
+        ////     Tipos de datos de texto
+        /****
+         *  CHAR
+         ***/
+        char caracter = '@';
+        JOptionPane.showMessageDialog(null, caracter);
+        /*****
+         *  STRING
+         *****/
+        String texto = "Siente el dolor, piensa en el dolor, acepta el dolor, conoce el dolor, ¡empuje celestial!\n" +"😞";
+        JOptionPane.showMessageDialog(null, texto);
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
