7cd63000df9d89720e64d4ddfb1a85be044783ad
Cristian <Kristian2964@gmail.com>
2025-10-01 11:02:27 -0500

    Estructuras de control segundo ejemplo

--- Estructuras_de_Control/src/com/fup/estructuras/Estructuras.java
+++ Estructuras_de_Control/src/com/fup/estructuras/Estructuras.java
@@ -17,9 +17,11 @@ public class Estructuras {
         /***
          * IF
          **/
+        
         Scanner tecla = new Scanner(System.in );
          //variable;
-        byte edad;
+         /**
+        byte edad=0;
         JOptionPane.showInputDialog("Ingresa la edad",edad);
 //                        edad = tecla.nextByte();
         final byte edadMayor =18;
@@ -28,6 +30,38 @@ public class Estructuras {
         }
         else{
              JOptionPane.showMessageDialog(null, "Es menor de edad");
+        }
+        **/
+        
+        //  EJEMPLO 2
+        
+        int num1,num2,num3;
+        String numeros = " ";
+        System.out.println("Ingrese el primer numero: ");
+        num1 = tecla.nextInt();
+        
+         System.out.println("Ingrese el segundo numero: ");
+        num2 = tecla.nextInt();
+        
+         System.out.println("Ingrese el tercer numero: ");
+        num3 = tecla.nextInt();
+        
+        System.out.println("===========================");
+        System.out.println("Los numeros digitados son: ");
+        numeros = Integer.toString(num1)+", "+Integer.toString(num2)+", "+Integer.toString(num3);
+        
+        System.out.println(numeros);
+        System.out.println("===========================");
+        
+        if(num1 > num2){
+            if(num1 > num3){
+                System.out.println(num1+" ES EL NUMERO MAYOR");
+            }
+        }else if (num2 > num3){
+                System.out.println(num2+" ES EL NUMERO MAYOR");
+        }else{
+            //System.out.println("=========================== "+num3);   
+                System.out.println(num3+" ES EL NUMERO MAYOR");
         }
     }
     
